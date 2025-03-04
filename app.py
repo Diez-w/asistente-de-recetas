b3b0de435b7ae41586307ccdaf47798e23f10261
Diez-w <diegozr614@gmail.com>
2025-03-04 14:23:37 -0500

    Primer commit - subida inicial

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify, render_template, request
+import pandas as pd
+
+app = Flask(__name__)
+
+# Cargar la base de datos de recetas (asegúrate de poner la ruta correcta)
+df = pd.read_csv(r"D:\BASE\conversacion_recetas.csv", encoding="utf-8", sep=";")
+  
+print(df.head())    # Esto imprimirá las primeras 5 filas del DataFrame
+
+
+@app.route("/")
+def index():
+    return render_template("index.html", recetas=df.to_dict(orient="records"))
+
+@app.route("/buscar", methods=["GET"])
+def buscar():
+    consulta = request.args.get("q", "").lower()
+    if consulta:
+        resultados = df[df["Título"].str.lower().str.contains(consulta, na=False)]
+    else:
+        resultados = df
+    return jsonify(resultados.to_dict(orient="records"))
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
